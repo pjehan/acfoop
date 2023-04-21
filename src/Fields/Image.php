eaec5916b38302a2cbde69ef6488b3610a272fd2
pjehan <pierre.jehan@gmail.com>
2023-04-21 12:42:21 +0200

    Add Required trait to Image Field

--- src/Fields/Image.php
+++ src/Fields/Image.php
@@ -5,11 +5,13 @@ namespace Pjehan\Acfoop\Fields;
 
 
 
+use Pjehan\Acfoop\Fields\Attributes\Required;
 use Pjehan\Acfoop\Fields\Attributes\ReturnFormat;
 
 class Image extends Field
 {
     use ReturnFormat;
+    use Required;
 
     public function getType(): string
     {
