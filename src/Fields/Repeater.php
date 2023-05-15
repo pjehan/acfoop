0de0e51aae04c2d70a3740dba687d4ef57bfdfad
pjehan <pierre.jehan@gmail.com>
2023-05-15 23:22:37 +0200

    Add Pagination to the Repeater field

--- src/Fields/Repeater.php
+++ src/Fields/Repeater.php
@@ -14,9 +14,48 @@ class Repeater extends Field
     use Layout;
     use MinMax;
 
+    private bool $pagination = false;
+    private int $rowsPerPage = 20;
+
     public function getType(): string
     {
         return 'repeater';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPagination(): bool
+    {
+        return $this->pagination;
+    }
+
+    /**
+     * @param bool $pagination
+     * @return Repeater
+     */
+    public function setPagination(bool $pagination): Repeater
+    {
+        $this->pagination = $pagination;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRowsPerPage(): int
+    {
+        return $this->rowsPerPage;
+    }
+
+    /**
+     * @param int $rowsPerPage
+     * @return Repeater
+     */
+    public function setRowsPerPage(int $rowsPerPage): Repeater
+    {
+        $this->rowsPerPage = $rowsPerPage;
+        return $this;
     }
 
     public function toArray(): array
@@ -25,7 +64,11 @@ class Repeater extends Field
             parent::toArray(),
             $this->subFieldsToArray(),
             $this->layoutToArray(),
-            $this->minMaxToArray()
+            $this->minMaxToArray(),
+            [
+                'pagination' => $this->isPagination() ? 1 : 0,
+                'rows_per_page' => $this->getRowsPerPage(),
+            ]
         );
     }
 }
