8f1cf93cbc4618636313a383283653d0879175de
pjehan <pierre.jehan@gmail.com>
2023-05-18 08:51:05 +0200

    Add maxLength to Text field

--- src/Fields/Text.php
+++ src/Fields/Text.php
@@ -14,18 +14,44 @@ class Text extends Field
     use Placeholder;
     use DefaultValue;
 
+    private ?int $maxLength = null;
+
     public function getType(): string
     {
         return 'text';
     }
 
+    /**
+     * @return int|null
+     */
+    public function getMaxLength(): ?int
+    {
+        return $this->maxLength;
+    }
+
+    /**
+     * @param int|null $maxLength
+     * @return Text
+     */
+    public function setMaxLength(?int $maxLength): Text
+    {
+        $this->maxLength = $maxLength;
+        return $this;
+    }
+
     public function toArray(): array
     {
-        return array_merge(
+        $array = array_merge(
             parent::toArray(),
             $this->placeholderToArray(),
             $this->requiredToArray(),
             $this->defaultValueToArray()
         );
+
+        if ($this->getMaxLength() !== null) {
+            $array['maxlength'] = $this->getMaxLength();
+        }
+
+        return $array;
     }
 }
