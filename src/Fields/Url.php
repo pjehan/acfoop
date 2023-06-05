4214614482fea4ea05779567398e44102cda27b8
pjehan <pierre.jehan@gmail.com>
2023-06-06 00:00:51 +0200

    Add defaultValue to Url Field

--- src/Fields/Url.php
+++ src/Fields/Url.php
@@ -4,11 +4,14 @@
 namespace Pjehan\Acfoop\Fields;
 
 
+use Pjehan\Acfoop\Fields\Attributes\DefaultValue;
 use Pjehan\Acfoop\Fields\Attributes\Placeholder;
 use Pjehan\Acfoop\Fields\Attributes\Required;
 
 class Url extends Field
 {
+
+    use DefaultValue;
     use Required;
     use Placeholder;
 
@@ -21,6 +24,7 @@ class Url extends Field
     {
         return array_merge(
             parent::toArray(),
+            $this->defaultValueToArray(),
             $this->placeholderToArray(),
             $this->requiredToArray()
         );
